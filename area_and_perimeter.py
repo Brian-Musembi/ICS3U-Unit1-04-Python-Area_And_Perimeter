7686b82056aa0917584d908c151c6f37dbb0d140
Brian Musembi <brian.musembi@mths.ca>
2021-04-23 14:01:54 +0000

    added area_and_perimeter.py

---
+++ area_and_perimeter.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+# Created by: Brian Musembi
+# Created on: 23 April 2021
+# This is the "Hello, World!" program, with proper style
+
+def main():
+    print("If a rectangle has the dimensions below:")
+    print("73m by 82m")
+    print("")
+    print("The area of the rectangle is {}m².".format(73 * 82))
+    print("And the perimeter of the rectangle is {}m.".format(2 * (73 + 82)))
+
+
+if __name__ == "__main__":
+    main()
